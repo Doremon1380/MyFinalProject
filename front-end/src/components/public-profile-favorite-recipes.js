d5b22ced59d89b4f98d9a9d7ff050d982315278c
Nguyen Van Tuan <cje.madlife2000@gmail.com>
2022-01-01 03:31:09 +0700

    112022OK

--- front-end/src/components/public-profile-favorite-recipes.js
+++ front-end/src/components/public-profile-favorite-recipes.js
@@ -21,78 +21,84 @@ function PublicProfileFavoriteRecipes() {
             <Container id="top-part-container-in-public-profile-favorite-recipes-page">
                 <PublicProfileTopPart />
             </Container>
+            <nav id="group-buttons-in-public-profile-favorite-recipes-page">
+                <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">About Me</button></a>
+                <a href="/www.foodshare.com/cook/cook-id/favorite-recipes/"><button className="group-button-in-public-profile-favorite-recipes-page selected-button-in-public-profile-favorite-recipes-page">Favorites</button></a>
+                <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">I Made It</button></a>
+                <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">Reviews</button></a>
+                <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">Personal Recipes</button></a>
+                <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">Followers</button></a>
+                <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page last-button-of-group-buttons-in-public-profile-favorite-recipes-page">Following</button></a>
+            </nav>
             <div id="public-profile-favorite-recipes-page">
-                <div>
-                    <div>
+                <div id="left-side-in-public-profile-favorite-recipes-page">
+                    <div id="about-me-in-public-profile-favorite-recipes-page">
                         <p>About me</p>
                         <p>(Discription)</p>
                         <p>My location</p>
                         <p>(Discription)</p>
                         <p>Connect with me</p>
-                        <p>(Discription)</p>
+                        <a href="#"><p>(Discription)</p></a>
                     </div>
-                    <div>
+                    <div id="followers-in-public-profile-favorite-recipes-page">
                         <p>Followers (number)</p>
                         <div>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <button>
-                                <i class="bi bi-plus"></i>
-                                <p>(number)</p>
-                            </button>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#">
+                                <button>
+                                    <i class="bi bi-plus"></i>
+                                    <p>(number)</p>
+                                </button>
+                            </a>
                         </div>
                     </div>
-                    <div>
+                    <div id="following-in-public-profile-favorite-recipes-page">
                         <p>Following (number)</p>
                         <div>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <a><img></img></a>
-                            <button>
-                                <i class="bi bi-plus"></i>
-                                <p>(number)</p>
-                            </button>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#"><img></img></a>
+                            <a href="#">
+                                <button>
+                                    <i class="bi bi-plus"></i>
+                                    <p>(number)</p>
+                                </button>
+                            </a>
                         </div>
                     </div>
                 </div>
-                <div>
-                    <nav id="group-buttons-in-public-profile-favorite-recipes-page">
-                        <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">About Me</button></a>
-                        <a href="/www.foodshare.com/cook/cook-id/favorite-recipes/"><button className="group-button-in-public-profile-favorite-recipes-page selected-button-in-public-profile-favorite-recipes-page">Favorites</button></a>
-                        <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">I Made It</button></a>
-                        <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">Reviews</button></a>
-                        <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">Personal Recipes</button></a>
-                        <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page">Followers</button></a>
-                        <a href="#"><button className="group-button-in-public-profile-favorite-recipes-page last-button-in-public-profile-favorite-recipes-page">Following</button></a>
-                    </nav>
-                    <div id="favorites-recipes-and-collections-in-public-profile-favorite-recipes-page">
-                        <a href="#">
-                            <button>Collections</button>
-                        </a>
-                        <a href="/recipe-details">
-                            <button>Recipes</button>
-                        </a>
-                    </div>
-                    <div id="favorites-recipes-sorting-options-in-public-profile-favorite-recipes-page">
-                        <label for="select-favorites-recipes-sorting-options-in-public-profile-favorite-recipes-page">Sort by:</label><br />
-                        <select name="sort types" id="select-made-recipes-sorting-options-in-public-profile-favorite-recipes-page">
-                            <option value="Newest">Newest</option>
-                            <option value="Rating">Rating</option>
-                            <option value="Title">Title</option>
-                        </select>
+                <div id="right-side-in-public-profile-favorite-recipes-page">
+                    <div id="top-section-of-right-side-in-public-profile-favorite-recipes-page">
+                        <div id="favorites-recipes-and-collections-in-public-profile-favorite-recipes-page">
+                            <a href="#">
+                                <button>Collections</button>
+                            </a>
+                            <a href="/recipe-details">
+                                <button>Recipes</button>
+                            </a>
+                        </div>
+                        <div id="favorite-recipes-sorting-options-in-public-profile-favorite-recipes-page">
+                            <label for="select-favorite-recipes-sorting-options-in-public-profile-favorite-recipes-page">Sort by:</label><br />
+                            <select name="sort types" id="select-favorite-recipes-sorting-options-in-public-profile-favorite-recipes-page">
+                                <option value="Newest">Newest</option>
+                                <option value="Rating">Rating</option>
+                                <option value="Title">Title</option>
+                            </select>
+                        </div>
                     </div>
                     <div id="recipes-in-public-profile-favorite-recipes-page">
                         <div className="recipe-in-public-profile-favorite-recipes-page">
